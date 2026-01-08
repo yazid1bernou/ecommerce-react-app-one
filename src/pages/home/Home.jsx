a96f9c1154deb2ef0be07c89e2018e9e54bdc4bf
unknown <yazid1bernou@gmail.com>
2026-01-08 21:30:14 +0100

    create FeaturedProduct and Card components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 
 import "./Home.scss";
 import Slider from "../../components/slider/Slider";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 
 const Home = () => {
     return (
         <div className="home">
             <Slider />
-
+            <FeaturedProducts type={"Featured"} />
+            <FeaturedProducts type={"Trending"} />
         </div>
     )
 }
