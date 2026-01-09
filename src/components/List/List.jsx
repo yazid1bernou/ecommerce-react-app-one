3138fd5208b1ce54a555bd058e4a4cb111c707f9
unknown <yazid1bernou@gmail.com>
2026-01-09 16:30:07 +0100

    create products component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,56 @@
+import "./List.scss";
+import Card from "../Card/Card";
+
+const List = ({ catId, maxPrice, sort }) => {
+
+    const data = [
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg",
+            img2: "https://images.pexels.com/photos/3966212/pexels-photo-3966212.jpeg",
+            title: "Item1",
+            isNew: true,
+            oldPrice: 19,
+            price: 13
+        },
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg",
+            img2: "",
+            title: "Item2",
+            isNew: true,
+            oldPrice: 21,
+            price: 12
+        },
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/1381558/pexels-photo-1381558.jpeg",
+            img2: "",
+            title: "Item3",
+            isNew: true,
+            oldPrice: 25,
+            price: 15
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg",
+            img2: "",
+            title: "Item4",
+            isNew: true,
+            oldPrice: 15,
+            price: 10
+        },
+
+    ]
+    return (
+        <div className="list">
+            {data?.map((item) => (
+                <Card item={item} key={item.id} />
+
+            ))}
+
+        </div>
+    )
+}
+
+export default List;
